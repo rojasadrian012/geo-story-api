5ec93701791c970fa94b25bed5727424a75ff941
rojasadrian012 <rojasadrian012@gmail.com>
2024-07-21 12:14:30 -0400

    seed by client

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { SeedService } from './seed.service';
@@ -6,14 +14,21 @@ import { Auth } from 'src/auth/decorators/auth.decorator';
 import { ValidRoles } from 'src/auth/interface/valid-roles';
 
 @ApiTags('Seed')
-
 @Controller('seed')
 export class SeedController {
-  constructor(private readonly seedService: SeedService) { }
+  constructor(private readonly seedService: SeedService) {}
 
   @Get()
   // @Auth(ValidRoles.superUser)
   executeSeed() {
+    return this.seedService.runSedd();
+  }
+
+  @Get('by-client')
+  @Auth(ValidRoles.superUser)
+  executeSeedByClient() {
+    console.log('by-client');
+
     return this.seedService.runSedd();
   }
 }
