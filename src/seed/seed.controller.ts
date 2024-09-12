aa9c6eed9f65acba34d751d3b3dcd5bd517e97e0
rojasadrian012 <rojasadrian012@gmail.com>
2024-09-11 21:39:36 -0400

    hidden seed path in controller

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -18,15 +18,15 @@ import { ValidRoles } from 'src/auth/interface/valid-roles';
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
-  @Get()
-  // @Auth(ValidRoles.superUser)
-  executeSeed() {
-    return this.seedService.runSedd();
-  }
+  // @Get()
+  // // @Auth(ValidRoles.superUser)
+  // executeSeed() {
+  //   return this.seedService.runSedd();
+  // }
 
-  @Get('by-client')
-  @Auth(ValidRoles.admin)
-  executeSeedByClient() {
-    return this.seedService.runSedd();
-  }
+  // @Get('by-client')
+  // @Auth(ValidRoles.admin)
+  // executeSeedByClient() {
+  //   return this.seedService.runSedd();
+  // }
 }
